81cc6f734d32c9858c405740073ed69d7354bf43
vera3588 <javeraa@eafit.edu.co>
2023-08-31 11:49:03 -0500

    auth

--- repon/autenticacion/models.py
+++ repon/autenticacion/models.py
@@ -1,3 +1,8 @@
-from django.db import models
+from __future__ import unicode_literals
 
-# Create your models here.
+from django.db import models
+from django.core.mail import send_mail
+from django.contrib.auth.models import PermissionsMixin
+from django.contrib.auth.base_user import AbstractBaseUser
+
+
--- repon/autenticacion/views.py
+++ repon/autenticacion/views.py
@@ -1,4 +1,22 @@
 from django.shortcuts import render
+from django.contrib import messages
+from .  import models
 
 def registro(request):
+    
+    if request.method == 'POST':
+        nombre = request.POST['nombre']
+        correo = request.POST['correo']
+        clave = request.POST['clave']
+        clave_repeat = request.POST['clave_repeat']
+        cargo = ""
+        empresa = ""
+
+        if clave != clave_repeat:
+            messages.info(request, 'Las contraseñas no coinciden')
+        else:
+            agregar = models.Usuario(nombre = nombre, correo = correo, cargo = cargo, empresa = empresa ,clave = clave)
+            agregar.save()
+            return render(request, 'autenticacion/registro.html')
+        
     return render(request, 'autenticacion/registro.html')
