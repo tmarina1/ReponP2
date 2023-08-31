37983bfb47e3dbd7640e934fe539624d6f30a251
vera3588 <javeraa@eafit.edu.co>
2023-08-30 20:12:44 -0500

    organizar urls

--- repon/repon/settings.py
+++ repon/repon/settings.py
@@ -37,7 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'autenticacion'
+    'autenticacion',
+    'empresas',
+    'inventario',
 ]
 
 MIDDLEWARE = [
--- repon/repon/urls.py
+++ repon/repon/urls.py
@@ -15,10 +15,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-from autenticacion import views as vistaAut
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', vistaAut.registro, name='registro')
+    path('', include('autenticacion.urls')),
+    path('inventario/', include('inventario.urls')),
+    path('empresa/', include('empresas.urls')),
 ]
