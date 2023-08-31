7f598be0b71617cae64a7242746f51ed680c97dc
jpyepes <jpyg123@gmail.com>
2023-08-30 19:52:17 -0500

    Creación de plantilla registro

--- repon/autenticacion/views.py
+++ repon/autenticacion/views.py
@@ -1,3 +1,4 @@
 from django.shortcuts import render
 
-# Create your views here.
+def registro(request):
+    return render(request, 'autenticacion/registro.html')
--- repon/repon/urls.py
+++ repon/repon/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from autenticacion import views as vistaAut
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', vistaAut.registro, name='registro')
 ]
