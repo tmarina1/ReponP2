37345f898572be17bfc1c9b913fef269a94e9125
jpyepes <jpyg123@gmail.com>
2023-08-31 16:17:16 -0500

    Correciones a registro

--- repon/autenticacion/urls.py
+++ repon/autenticacion/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     path('', views.registro, name="registro"),
+    path('inicioSesion/', views.inicioSesion, name="inicioSesion"),
 ]
--- repon/autenticacion/views.py
+++ repon/autenticacion/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render
 from django.contrib import messages
 from django.contrib.auth.models import User
+from django.shortcuts import redirect
 from . import models
 
 def registro(request):
-    
+    colorMensaje = True
+    mensaje = ''
     if request.method == 'POST':
         nombre = request.POST['nombre']
         correo = request.POST['correo']
@@ -12,14 +14,21 @@ def registro(request):
         claveRepetida = request.POST['claveRepetida']
         administrador = True
         if clave != claveRepetida:
-            messages.warning(request, 'Las contraseñas no coinciden')
+            mensaje = 'Las contraseñas no coinciden'
         else:
-            usuario = User.objects.create_user(first_name=nombre,username=correo, email=correo, password=clave)
-            usuario.save()
-            perfil,creado = models.Perfil.objects.get_or_create(usuario=usuario)
-            if creado:
-                perfil.cargo = administrador
-                perfil.save()
-            messages.success(request, 'Usuario creado exitosamente')
+            try:
+                usuario = User.objects.create_user(first_name=nombre,username=correo, email=correo, password=clave)
+                usuario.save()
+                perfil,creado = models.Perfil.objects.get_or_create(usuario=usuario)
+                if creado:
+                    perfil.cargo = administrador
+                    perfil.save()
+                return redirect(inicioSesion)
+            except:
+               mensaje = 'Ya existe un usuario registrado con este correo'
+               colorMensaje = False
         
-    return render(request, 'autenticacion/registro.html')
+    return render(request, 'autenticacion/registro.html', {'colorMensaje':colorMensaje,'mensaje':mensaje})
+
+def inicioSesion(request):
+    return render(request,'autenticacion/inicioSesion.html')
