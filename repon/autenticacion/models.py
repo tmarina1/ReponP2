565544a74e8e08723fadaf9629e06a5cdaf430c7
jpyepes <jpyg123@gmail.com>
2023-08-31 14:41:02 -0500

    Modelo perfil para rellenar cargo

--- repon/autenticacion/models.py
+++ repon/autenticacion/models.py
@@ -1,3 +1,6 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-
+class Perfil(models.Model):
+    usuario = models.OneToOneField(User, on_delete=models.CASCADE)
+    cargo = models.BooleanField(default=False)
--- repon/autenticacion/views.py
+++ repon/autenticacion/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.contrib import messages
 from django.contrib.auth.models import User
-from .  import models
+from . import models
 
 def registro(request):
     
@@ -10,12 +10,16 @@ def registro(request):
         correo = request.POST['correo']
         clave = request.POST['clave']
         claveRepetida = request.POST['claveRepetida']
-        administrador = 1
+        administrador = True
         if clave != claveRepetida:
             messages.warning(request, 'Las contraseñas no coinciden')
         else:
-            usuario = User.objects.create_user(first_name=nombre,username=correo, email=correo, password=clave,is_superuser=administrador)
+            usuario = User.objects.create_user(first_name=nombre,username=correo, email=correo, password=clave)
             usuario.save()
+            perfil,creado = models.Perfil.objects.get_or_create(usuario=usuario)
+            if creado:
+                perfil.cargo = administrador
+                perfil.save()
             messages.success(request, 'Usuario creado exitosamente')
         
     return render(request, 'autenticacion/registro.html')
