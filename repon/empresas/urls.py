c1892bfe7432044f0deb39625c921073262c6a2a
mmc31-08 <mateo-31-cadavid@outlook.com>
2023-09-01 15:49:08 -0500

    diseño forms sin data

--- repon/empresas/urls.py
+++ repon/empresas/urls.py
@@ -1,3 +1,6 @@
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path,include
 from . import views
@@ -6,3 +9,7 @@ urlpatterns = [
     path('', views.Empresas),
     path('crearEmpresa/', views.CrearEmpresas),
 ]
+
+if settings.DEBUG:
+    urlpatterns += staticfiles_urlpatterns()
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
