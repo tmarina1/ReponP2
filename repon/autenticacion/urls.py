8e5e8f5a01e60a60b254aeb3b9aa3b547656dad8
vera3588 <javeraa@eafit.edu.co>
2023-09-04 21:27:18 -0500

    quitar coordinador

--- repon/autenticacion/urls.py
+++ repon/autenticacion/urls.py
@@ -4,7 +4,6 @@ from django.contrib.auth.views import PasswordResetView, PasswordResetDoneView, 
 urlpatterns = [
     path('', views.registro, name="registro"),
     path('inicioSesion/', views.inicioSesion, name="inicioSesion"),
-    path('inicioSesionCoordinador/', views.inicioSesionCoordinador, name="inicioSesionCoordinador"),
     path('reset/password_reset', PasswordResetView.as_view(template_name='recuperacion/recuperarClave.html', email_template_name="recuperacion/correoRecuperarClave.html"), name = 'password_reset'),
     path('reset/password_reset_done', PasswordResetDoneView.as_view(template_name='recuperacion/recuperarClaveEstado.html'), name = 'password_reset_done'),
     path('reset/<uidb64>/<token>/', PasswordResetConfirmView.as_view(template_name='recuperacion/recuperarClaveFormulario.html'), name = 'password_reset_confirm'),
