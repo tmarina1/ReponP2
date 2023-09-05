9679fbaba805545521a391d39f702bbcd8d61464
vera3588 <javeraa@eafit.edu.co>
2023-09-04 22:35:44 -0500

    registrar empresa

--- repon/administracion/models.py
+++ repon/administracion/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class Empresa(models.Model):
+    nit = models.CharField(max_length=50)
+    nombreEmpresa = models.CharField(max_length=50)
+    direccion = models.CharField(max_length=100)
+    correo = models.CharField(max_length=100)
+    telefono = models.CharField(max_length=50)
+
--- repon/administracion/views.py
+++ repon/administracion/views.py
@@ -1,8 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from autenticacion import models as autenticacion
+from . import models
 
-# Create your views here.
 def Empresas(request):
     return render(request, "empresas.html")
 
 def CrearEmpresas(request):
+    if request.method == 'POST':
+
+        nit = request.POST['nit']
+        nombreEmpresa = request.POST['nombreEmpresa']
+        direccion = request.POST['direccion']
+        correo = request.POST['correo']
+        telefono = request.POST['telefono']        
+
+        registroEmpresa = models.Empresa.objects.create(nit = nit, nombreEmpresa = nombreEmpresa, direccion = direccion,
+                                                        correo = correo, telefono = telefono)
+        registroEmpresa.save()
+        
+        asignacionEmpresa = autenticacion.Perfil.objects.get()
+
+        return redirect(CrearEmpresas)
+    
     return render(request, "crearEmpresas.html")
