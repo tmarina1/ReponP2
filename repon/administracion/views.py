c993053fd3d98d85507212ddfbeedbfcccc077c8
vera3588 <javeraa@eafit.edu.co>
2023-09-06 16:18:13 -0500

    verificacion si existe la empresa

--- repon/administracion/views.py
+++ repon/administracion/views.py
@@ -7,22 +7,26 @@ def empresas(request):
     return render(request, "empresas.html")
 
 def crearEmpresas(request):
-    if request.method == 'POST':
+    idUsuario = request.user.id
+    empresa = models.Empresa.objects.filter(usuarioVinculado_id = idUsuario).exists()
 
-        nit = request.POST['nit']
-        nombreEmpresa = request.POST['nombreEmpresa']
-        direccion = request.POST['direccion']
-        correo = request.POST['correo']
-        telefono = request.POST['telefono']
-        idUsuario = request.user.id
-
-        idUsuarioAutenticado = autenticacion.Perfil.objects.filter(id = idUsuario).values_list('id', flat= True)
-        Usuario = User.objects.filter(id = idUsuarioAutenticado[0]).values_list('first_name', flat=True)
-        
-        print(Usuario[0])  
-        registroEmpresa = models.Empresa.objects.create(nit = nit, nombreEmpresa = nombreEmpresa, direccion = direccion,
-                                                        correo = correo, telefono = telefono, usuarioVinculado = idUsuarioAutenticado[0])
-        registroEmpresa.save()
+    if empresa:
         return redirect(empresas)
-    
-    return render(request, "crearEmpresas.html")
+
+    else:
+        if request.method == 'POST':
+
+            nit = request.POST['nit']
+            nombreEmpresa = request.POST['nombreEmpresa']
+            direccion = request.POST['direccion']
+            correo = request.POST['correo']
+            telefono = request.POST['telefono']
+
+            idUsuarioAutenticado = autenticacion.Perfil.objects.filter(id = idUsuario).values_list('id', flat= True)
+            
+            registroEmpresa = models.Empresa.objects.create(nit = nit, nombreEmpresa = nombreEmpresa, direccion = direccion,
+                                                            correo = correo, telefono = telefono, usuarioVinculado_id = idUsuarioAutenticado[0])
+            registroEmpresa.save()
+            return redirect(empresas)
+        
+        return render(request, "crearEmpresas.html")
