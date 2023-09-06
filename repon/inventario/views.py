766ad634daae7ea5f72af9cc920006e07aa0da7c
mmc31-08 <mateo-31-cadavid@outlook.com>
2023-09-06 13:46:31 -0500

    formulario_1 inventario

--- repon/inventario/views.py
+++ repon/inventario/views.py
@@ -2,3 +2,6 @@ from django.shortcuts import render
 
 def Inventario(request):
     return render(request, "inventario.html")
+
+def crearInventario(request):
+    return render(request, "crearInventario.html")
