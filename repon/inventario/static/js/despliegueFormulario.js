b9fed0581a39758dcf9d3c2de0de3ed1b865617d
mmc31-08 <mateo-31-cadavid@outlook.com>
2023-09-06 23:17:27 -0500

    formulario crear inventario

--- repon/inventario/static/js/despliegueFormulario.js
+++ repon/inventario/static/js/despliegueFormulario.js
@@ -2,10 +2,17 @@
 formulario1 = document.getElementById("formulario1");
 formulario2 = document.getElementById("formulario2");
 botonContinuar = document.getElementById("btn-continuar");
+botonVolver = document.getElementById("btn-volver");
 
 botonContinuar.addEventListener("click", this.mostrarFormulario2.bind(this));
+botonVolver.addEventListener("click", this.mostrarFormulario1.bind(this));
 
-function mostrarFormulario2(){
+var mostrarFormulario2 = function(){
     formulario1.style.display = "none";
     formulario2.style.display = "block";
+}
+
+function mostrarFormulario1(){
+    formulario2.style.display = "none";
+    formulario1.style.display = "block";
 }
