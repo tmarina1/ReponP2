a39c20b4ef9f364e6c878c1e2d403bdcceeab93a
juan <jpyg123@gmail.com>
2023-09-09 18:27:47 -0500

    Correciones en formulario crearEmpresa

--- repon/administracion/models.py
+++ repon/administracion/models.py
@@ -3,8 +3,8 @@ from autenticacion import models as usuarios
 class Empresa(models.Model):
     nit = models.CharField(max_length=50)
     nombreEmpresa = models.CharField(max_length=50)
-    direccion = models.CharField(max_length=100)
-    correo = models.CharField(max_length=100)
-    telefono = models.CharField(max_length=50)
+    direccion = models.CharField(max_length=100, default='NA')
+    departamento = models.CharField(max_length=100)
+    ciudad = models.CharField(max_length=100)
     usuarioVinculado = models.ForeignKey(usuarios.Perfil, on_delete=models.CASCADE, null=True)
 
--- repon/administracion/views.py
+++ repon/administracion/views.py
@@ -24,15 +24,15 @@ def crearEmpresas(request):
         if request.method == 'POST':
 
             nit = request.POST['nit']
-            nombreEmpresa = request.POST['nombreEmpresa']
+            nombreEmpresa = request.POST['nombre']
             direccion = request.POST['direccion']
-            correo = request.POST['correo']
-            telefono = request.POST['telefono']
+            departamento = request.POST['departamento']
+            ciudad = request.POST['ciudad']
 
             idUsuarioAutenticado = autenticacion.Perfil.objects.filter(id = idUsuario).values_list('id', flat= True)
             
             registroEmpresa = models.Empresa.objects.create(nit = nit, nombreEmpresa = nombreEmpresa, direccion = direccion,
-                                                            correo = correo, telefono = telefono, usuarioVinculado_id = idUsuarioAutenticado[0])
+                                                            departamento = departamento, ciudad = ciudad, usuarioVinculado_id = idUsuarioAutenticado[0])
             registroEmpresa.save()
             return redirect(landingAdmon)
         
