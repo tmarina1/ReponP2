7eeb896f0145f8e4416788866dd491b08c479a74
jpyepes <jpyg123@gmail.com>
2023-09-11 11:09:49 -0500

    Inicio modelo insumo

--- repon/inventario/models.py
+++ repon/inventario/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from datetime import datetime
 
 # Create your models here.
+
+class Insumo(models.Model):
+    codigo = models.CharField(max_length=15)
+    referencia = models.CharField(max_length=150)
+    unidad = models.CharField(max_length=100)
+    cantidad = models.DecimalField(max_digits=10, decimal_places=3)
+    valorUnitario = models.DecimalField(max_digits=10, decimal_places=2)
+    impuesto = models.DecimalField(max_digits=10, decimal_places=2,default='19')
+    nombreMarca = models.CharField(max_length=200)
+    nombreProyecto = models.CharField(max_length=200)
+    tipoInsumo = models.CharField(max_length=200)
+    ubicacion = models.CharField(max_length=200)
+    fechaIngreso = models.DateTimeField(auto_now_add=True)
+    fechaCaducidad = models.DateTimeField(default='')
+    fechaCompra = models.DateTimeField(default=datetime.now)
+    observaciones = models.CharField(max_length=200) 
