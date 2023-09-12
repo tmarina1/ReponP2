9315f3294a4f3de6ea4f04f5ebcdf7bcf369c5b2
jpyepes <jpyg123@gmail.com>
2023-09-12 18:38:19 -0500

    Adelanto views crearInventario

--- repon/inventario/views.py
+++ repon/inventario/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from .models import Insumo, Proyecto
 
 def inventario(request):
     return render(request, "inventario.html")
@@ -10,4 +11,24 @@ def opcionesCoordinador(request):
     return render(request, "opcionesCoordinador.html")
 
 def crearInventario(request):
+    if request.method == 'post':
+        codigo = request.POST['codigo']
+        ref = request.POST['ref']
+        unidadBase = request.POST['unidadBase']
+        cantidad = request.POST['cantidad']
+        valorU = request.POST['valorU']
+        iva = request.POST['iva']
+        marca = request.POST['marca']
+        tipoInsumo = request.POST['tipoInsumo']
+        lugarAlmacenado = request.POST['lugarAlmacenado']
+        fechaCaducidad = request.POST['fechaCaducidad']
+        fechaCompra = request.POST['fechaCompra']
+        observaciones = request.POST['observaciones']
+
+        proyectoId = 1
+        crearInsumo = Insumo.objects.create(codigo = codigo, referencia = ref, unidad = unidadBase, cantidad = cantidad,
+                                            valorUnitario = valorU, impuesto = iva, nombreMarca = marca,
+                                            tipoInsumo = tipoInsumo, ubicacion = lugarAlmacenado, fechaCaducidad = fechaCaducidad,
+                                            fechaCompra = fechaCompra, observaciones = observaciones, proyectoAsociado = proyectoId )
+
     return render(request, "crearInventario.html")
