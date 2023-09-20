9ab90489222dff94bf9a4e8c7dc0e42f29d7110f
vera3588 <javeraa@eafit.edu.co>
2023-09-20 16:10:20 -0500

    correcion puerto para envio email

--- repon/repon/settings.py
+++ repon/repon/settings.py
@@ -135,7 +135,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 25
+EMAIL_PORT = 587
 EMAIL_HOST_USER = 'pasteamdev1@gmail.com'
 EMAIL_HOST_PASSWORD = 'ekac rcro vgom vdma'
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
